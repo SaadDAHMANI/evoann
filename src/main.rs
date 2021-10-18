7a0977d17793ab50167d35ce230285826654fb1e
SaadDAHMANI <tahatatakor7@gmail.com>
2021-10-18 19:10:54 +0100

    test update_weights is Ok.
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ include!("eo_trainer.rs");
 fn main() {
     println!("Hello, world!");
 
-    let layers:Vec<usize> = vec!{3,2,1};
+    let layers:Vec<usize> = vec!{3,4,2};
     let activations:Vec<Activations> = vec!{Activations::TanH, Activations::Linear };
     let mut nn = Neuralnet::new(layers, activations);
     
@@ -14,7 +14,12 @@ fn main() {
      //   neuralnet : nn,
     //};   
     
-    let w = vec![0.0f32; nn.get_weights_count()];
+    let mut w = vec![0.0f32; nn.get_weights_count()];
+
+    for i in 0..w.len() {
+        w[i]=i as f32;
+    }
+    
 
     println!("W before update = {:?}", nn.weights);
 
