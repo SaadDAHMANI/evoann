6a70c4f707d49bf0c2b1d94ae6ee3eb8bc2d4bf8
SaadDAHMANI <tahatatakor7@gmail.com>
2022-01-29 21:37:36 +0100

    add streamflow forecasting test
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -83,12 +83,176 @@ fn main() {
         } 
 
         {
-            test_water_quality();
+            streamflow_forecast();
+           //test_water_quality();
            // test_water_quality_loop();           
             
         }
 
 }
+
+
+fn streamflow_forecast(){
+
+    //let path =String::from("/home/sd/Documents/AppDev/Rust/evoann/data/2Qm1.csv");
+    let path =String::from("/home/sd/Documents/AppDev/Rust/evoann/data/Coxs2.csv");
+    
+    let mut incols = Vec::new();
+    incols.push(2);
+    incols.push(3);
+    
+    let mut outcols = Vec::new();
+    outcols.push(1);
+    
+    let learn_part : usize = 4868; 
+    
+   let ds0 =  Dataset::read_from_csvfile(&path, &incols, &outcols);
+
+   let (ds_learn, ds_test) = ds0.get_shuffled().split_on_2(learn_part);
+   let lcount = ds_learn.inputs.len();
+
+   let ds_learn2 = ds_learn.clone();
+
+   //println!("dataset = {:?}", ds);
+
+   println!("----------------STREAMFLOW prediction-------------");
+
+   let chronos = Instant::now();
+
+   //println!("shuffled ataset = {:?}", ds.get_shuffled());            
+    
+   let layers:Vec<usize> = vec!{incols.len(), 5, outcols.len()};
+   let annstruct = layers.clone();
+   let activations:Vec<Activations> = vec!{Activations::Sigmoid, Activations::Linear};
+   let mut nnet = Neuralnet::new(layers, activations); 
+                 
+   //println!("In : {:?}", data_in);
+   //println!("Out : {:?}", data_out);      
+   let p_size : usize = 30;
+   let k_max : usize = 1000;
+   let ub : f64 = 5.0;
+   let lb : f64 = -5.0;
+
+   // let mut eoann = SequentialEOTrainer::new(&mut nnet, ds_learn.inputs, ds_learn.outputs, p_size, k_max, lb, ub);
+   let mut eoann = SequentialPSOTrainer::new(&mut nnet, ds_learn.inputs, ds_learn.outputs, p_size, k_max, lb, ub);
+   // set EO params ----------
+   // eoann.a1 = 2.0; 
+   // eoann.a2 = 1.0;  
+   // eoann.gp = 0.5;
+   //-------------------------
+           // set EO params ----------
+   eoann.c1 = 2.0; 
+   eoann.c2 = 2.0;        
+   //-------------------------
+  
+
+   let (_a, _wbi, _c) = eoann.learn();
+   
+   println!("_Learning items count : {:?}",lcount);
+   println!("*Population-size : {},  *ANN-Struct :{:?}", p_size, annstruct);
+
+   println!("_");
+   
+   println!("WQ - final Learning error : RMSEl = {:?}", _a );
+   
+   println!("_"); 
+    {     
+        
+         //println!("Writing optimization curve ...");
+         let pathcrv =String::from("/home/sd/Documents/AppDev/Rust/evoann/data/Coxs_convergenceTrnd_EO.csv");
+         //let pathcrv =String::from("/home/roua/Documents/Rust/evoann/data/Coxs_convergenceTrnd_EO.csv");
+         
+         let head = String::from("Coxs_RMSE-Cnvergence_Trend_EO");
+         let _error = Dataset::write_to_csv(&pathcrv, &Some(head), &_c);
+         println!("Writing optimization curve finish.");
+    
+         //println!("---------------------TESTING-----------------------"); 
+     
+       //println!("Real [Wi] = {:?}", nnet.weights);
+
+        //println!("Real [bi] = {:?}", nnet.biases);
+
+        //println!("_");
+
+         //println!("[nnet] -> testing result [Ca], [Mg(]= {:?}) --> {:?}", test, nnet.feed_forward(&test));
+         let computed_learn = eoann.compute_out_for2(&ds_learn2.inputs);  
+         let computedlearn = convert2vector(&computed_learn);
+         let observedlearn = convert2vector(&ds_learn2.outputs);   
+         let _r2l =   Dataset::compute_determination_r2(&computedlearn, &observedlearn);
+
+         println!("WQ - final Learning determination coef. : R2l = {:?}", _r2l);   
+         println!("_");   
+         
+         println!("writing learning results .....");
+              
+         let pathlearn  = String::from("/home/sd/Documents/AppDev/Rust/evoann/data/Coxs_dataset_learn_results_EO.csv");             
+         //let pathlearn  = String::from("/home/roua/Documents/Rust/evoann/data/Coxs_dataset_learn_results_EO.csv");             
+         
+         let mut headers = Vec::new();
+         headers.push(String::from("Computed CE"));
+         let _error = Dataset::write_to_csv2(&pathlearn, &Some(headers), &computed_learn);
+         println!("Writing learning results ......OK");
+
+        
+         let computed_test = eoann.compute_out_for2(&ds_test.inputs);
+
+         let computed = convert2vector(&computed_test);
+         let observed = convert2vector(&ds_test.outputs);
+         let rmse_test = Dataset::compute_rmse(&computed, &observed);
+         println!("WQ - final Testing error : RMSEt = {:?}", rmse_test);
+
+         let _r2t =   Dataset::compute_determination_r2(&computed, &observed);
+         println!("WQ - final Testing determination coef : R2t = {:?}", _r2t);   
+        
+         println!("Writing test results ......");
+         
+         let pathtest =String::from("/home/sd/Documents/AppDev/Rust/evoann/data/Coxs_dataset_test_results_EO.csv");
+         //let pathtest =String::from("/home/roua/Documents/Rust/evoann/data/Coxs_dataset_test_results_EO.csv");
+         
+         
+         let mut headers= Vec::new();
+         headers.push(String::from("Computed CE-Test"));
+         println!("test elements :  {:?}", computed_test.len());   
+         let _error = Dataset::write_to_csv2(&pathtest, &Some(headers), &computed_test);
+         println!("Writing test results finish ...... OK.");
+    
+    
+    //for rs in result.iter() {
+     //   println!("{}", rs[0]);
+    //}
+   
+    let duration = chronos.elapsed();
+    println!("End computation in : {:?}.", duration);
+
+    {
+        let _path =String::from("/home/sd/Documents/AppDev/Rust/evoann/data/Coxs3_inputs.csv");
+        //let _path =String::from("/home/roua/Documents/Rust/evoann/data/Coxs3_inputs.csv");
+
+        let mut _incols = Vec::new();
+        _incols.push(2);
+        _incols.push(3);
+        
+        let mut _outcols = Vec::new();
+        _outcols.push(1);
+        
+        let _dss =  Dataset::read_from_csvfile(&_path, &_incols, &_outcols);
+        let _computed = eoann.compute_out_for2(&_dss.inputs);
+
+        let mut _headers= Vec::new();
+        _headers.push(String::from("Computed QC"));
+
+        let _path_result =String::from("/home/sd/Documents/AppDev/Rust/evoann/data/Coxs_dataset_test_results_EO.csv");
+        //let _path_result =String::from("/home/roua/Documents/Rust/evoann/data/Coxs_dataset_test_results_EO.csv");
+
+        let _error = Dataset::write_to_csv2(&_path_result, &Some(_headers), &_computed);
+         println!("Writing results is finish ...... OK.");
+        
+    }
+
+}
+
+}
+
 
 fn test_water_quality(){
 
@@ -136,9 +300,6 @@ fn test_water_quality(){
        let k_max : usize = 3000;
        let ub : f64 = 5.0;
        let lb : f64 = -5.0;
-
-       // add coment 
-       let x : i32 = 3;
 
        let mut eoann = SequentialEOTrainer::new(&mut nnet, ds_learn.inputs, ds_learn.outputs, p_size, k_max, lb, ub);
        //let mut eoann = SequentialPSOTrainer::new(&mut nnet, ds_learn.inputs, ds_learn.outputs, p_size, k_max, lb, ub);
